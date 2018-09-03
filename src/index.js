44d553cd4d775f6cb6e9a735dd78fe7caa6be2c7
林哲源 <linzheyuan@ebaolife.com>
2018-09-03 09:08:54 +0800

    更改

--- src/index.js
+++ src/index.js
@@ -1,93 +1,63 @@
-import React from 'react';
-import produce from 'immer';
-import equal from 'fast-deep-equal';
+import React from 'react'
 
-class Wrapper extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    return !equal(this.props.data, nextProps.data);
-  }
-  render() {
-    return this.props.fn(this.props.data);
-  }
-}
-
-export function init(_store) {
-  let store = _store;
-  function createSubscribe() {
-    const listener = [];
-    return {
-      listen: updator => listener.push(updator),
-      unListen: l => listener.splice(listener.indexOf(l), 1),
-      update: updator => {
-        for (let i = 0; i < listener.length; i++) {
-          listener[i](updator);
-        }
-        if (listener.length === 0) {
-          if (typeof updator === 'function') {
-            const newState = produce(store, draft => {
-              updator(draft);
-            });
-            if (!equal(newState, store)) {
-              store = newState;
-            }
-            return;
-          }
-        }
-      },
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: {},
     };
+    this.imgs = [];
+    this.preloadImgs(props.imgs);
   }
-  const {listen, unListen, update} = createSubscribe();
-
-  class Provider extends React.PureComponent {
-    constructor(props) {
-      super(props);
-      this.state = store;
-    }
-
-    reRender = updator => {
-      if (typeof updator === 'function') {
-        const newState = produce(this.state, draft => {
-          updator(draft);
-        });
-        if (!equal(newState, this.state)) {
-          this.setState(newState);
-          store = newState;
-        }
+  componentWillReceiveProps(nextProps) {
+    let { imgs } = nextProps;
+    this.preloadImgs(imgs);
+  }
+  preloadImgs = imgs => {
+    let {  srcName = 'src', keyName = 'id' } = this.props;
+    imgs.forEach(img => {
+      let { [keyName]: id } = img;
+      if (this.state.loaded[id]) {
         return;
       }
-    };
-    componentWillUnmount() {
-      unListen(this.reRender);
-    }
-
-    componentDidMount() {
-      listen(this.reRender);
-    }
-
-    render() {
-      return this.props.children(this.state);
-    }
-  }
-
-  return {
-    Store: () => store,
-    Ctx: props => {
-      if (typeof props !== 'function') {
-        return <Provider>{props.children}</Provider>;
+      if (this.imgs.some(item => item[keyName] === img[keyName])) {
+        return;
       }
-      return <Provider>{props}</Provider>;
-    },
-    Put: update,
-    Auto: selector => {
-      return fn => {
-        return (
-          <Provider>
-            {state => {
-              return <Wrapper data={selector(state)} fn={fn} />;
-            }}
-          </Provider>
-        );
-      };
-    },
+      this.imgs.push(img);
+      if (this.preloading) {
+        return;
+      }
+      this.preload(this.imgs.length - 1);
+    });
   };
+  preload = index => {
+    let {  srcName = 'src', keyName = 'id' } = this.props;
+    index = index || 0;
+    if (index >= this.imgs.length) {
+      this.preloading = false;
+      return false;
+    }
+    this.preloading = true;
+    let { [keyName]: id } = this.imgs[index];
+    let oImg = new Image();
+    oImg.onload = () => {
+      this.setState({
+        loaded: {
+          ...this.state.loaded,
+          [id]: true,
+        },
+      });
+      this.preload(index + 1);
+    };
+    oImg.onerror = () => {
+      this.preload(index + 1);
+    };
+    oImg.src = this.imgs[index].src;
+  };
+  render() {
+    let { imgs, defaultSrc = '', srcName = 'src', keyName = 'id' } = this.props;
+    let imgs=imgs.map(item => ({ ...item, [srcName]: this.state.loaded[item[keyName]] ? item[srcName] : defaultSrc }))
+    return this.props.children(imgs)
+  }
 }
+export default Index;
